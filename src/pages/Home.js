de3de91ea5f6025799d7bbc2d9d2155d98c7f3ad
anildalar <dollorinfotech@gmail.com>
2023-10-06 10:02:59 +0530

    new

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import { Box, Typography, styled } from '@mui/material'
+import React from 'react'
+
+
+/* const Wrapper  = styled(<Box />)(({theme})=>({
+  background:"red",
+})); */
+const Heading = styled(<Typography  />);
+
+export default function Home() {
+  return (
+        <Heading variant="h1">
+          Hello 
+        </Heading>
+  )
+}
---
+++ test.js
@@ -0,0 +1,13 @@
+
+//1. Function define
+function anil(a){
+    return a+10;
+    //console.log('Hello');
+}
+
+//2. Function calling/invocation
+let x = anil(5)(({theme})=>{
+    
+});
+
+console.log(x+1);
